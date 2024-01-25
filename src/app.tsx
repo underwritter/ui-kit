5b1938939f86dd8a6cc74380ce1e828199856658
underwritter <ntpp96@mail.ru>
2024-01-25 18:07:04 +0300

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+function liveDangerously(x: number | null) {
+  // No error
+  console.log(x!.toFixed());
+}
+
+liveDangerously(null)
+
 function App() {
   return <div className="app" style={{display: "grid", gap: "10px"}}></div>;
 }
